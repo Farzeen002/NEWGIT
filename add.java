9cf49d8b0989ebaad6923c653f53025d260a1d9b
Farzeen Ahmed <farzeenahmed2000@gmail.com>
2023-05-02 12:22:24 +0530

    complete

--- add.java
+++ add.java
@@ -1,8 +1,17 @@
 public class add {
 
-    int a,b,c;
+        public static void main(String[] args) {
+            
+            int a,b,c;
 
-    c=a+b;
+            a=10;
+            b=20;
+
+            c=a+b;
+
+            System.out.println(c);
+        }
+    
 
     
     
